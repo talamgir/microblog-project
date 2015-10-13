a2b70b11fde29489437e797348bd30293e197e3f
Taimur Alamgir <talamgir@law.gwu.edu>
2015-10-13 14:12:56 -0400

    First Commit

---
+++ db/migrate/20151013161203_user_table.rb
@@ -0,0 +1,8 @@
+class UserTable < ActiveRecord::Migration
+  def change
+	create_table :users do |t|
+      # t.integer :id
+      t.string :name
+     end
+  end
+end
---
+++ models.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+end
+
+class Handle < ActiveRecord::Base
+end
